dba3e9c8891b7662753b1513dd1abde64b331c5c
sinshr <98868456+sinnshr@users.noreply.github.com>
2025-09-12 22:45:41 +0330

    refactor: restructure App component and update routing
    - Replaced Main component with Home in the routing structure.
    - Enhanced CSS with Tailwind and added custom font styles.

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,24 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
-import Calendar from './components/Calendar.jsx';
-import Main from './components/Main.jsx';
-import Notes from './components/Notes.jsx';
-import Timer from './components/Timer.jsx';
-import About from './components/About.jsx';
-import { motion } from "framer-motion";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Timer from "./components/Timer";
+import Calendar from "./components/Calendar";
+import Note from "./components/Note";
+
+import { Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
-    <>
-      <div className="app">
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/timer" element={<Timer />} />
-          <Route path="/calendar" element={<Calendar />} />
-          <Route path="/notes" element={<Notes />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
-      <motion.div
-        style={{
-          position: "fixed",
-          top: 0,
-          left: 0,
-          width: "100vw",
-          height: "100vh",
-          background: "linear-gradient(45deg, var(--hunyadi-yellow), var(--moss-green))",
-          zIndex: -1,
-        }}
-        onMouseMove={(e) => {
-          const x = (e.clientX / window.innerWidth) * 100;
-          const y = (e.clientY / window.innerHeight) * 100;
-          e.currentTarget.style.background = `radial-gradient(circle at ${x}% ${y}%, var(--hunyadi-yellow), var(--moss-green))`;
-        }}
-      >
-        {/* Your app content goes here */}
-        <div style={{ position: "relative", zIndex: 1 }}>
-          <h1></h1>
-        </div>
-      </motion.div>
-    </>
-  );
+    <div className="w-full h-screen flex flex-col justify-center items-center p-5 max-w-[90%] m-auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="timer" element={<Timer />} />
+        <Route path="calendar" element={<Calendar />} />
+        <Route path="note" element={<Note />} />
+      </Routes>    
+    </div>
+  )
 }
 
-export default App;
+export default App
